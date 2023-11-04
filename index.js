01d03bd67a712336c16a8bc0acd736461e5cf0c1
Zafar12345678 <127655821+Zafar12345678@users.noreply.github.com>
2023-11-04 09:22:40 +0530

    shailun commite

--- index.js
+++ index.js
@@ -22,4 +22,20 @@ app.use("/api", basic_route);
 
 app.listen(5000, function() {
     console.log("Server is ready on port 5000");
-});
+}); 
+
+// const express = require("express");
+// const app = express();
+// const cors  = require("cors");
+// const mongoose = require("mongoose");
+// mongoose.connect("mongodb+srv://merningday123:merningday123@cluster0.jtxz4yj.mongodb.net/software?retryWrites=true&w=majority")
+
+// app.use(cors());
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// db.once('open', function() {
+//     console.log('Connected to MongoDB');
+// });
+// app.listen(5000, function() {
+//     console.log("Server is ready on port 5000");
+// }); 
